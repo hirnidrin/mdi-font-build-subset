b78fe912d94a447b497c3bb06ec97d229dccd166
hirnidrin <info@itsu.ch>
2021-07-04 23:34:45 +0200

    Initial commit of working version.

---
+++ README.md
@@ -0,0 +1,31 @@
+# mdi-font-build-subset
+
+## Synopsis
+
+Dev helper package to build a reduced version of the [Material Design Icons](https://materialdesignicons.com/) webfont:
+* Skips ttf and eot fonts, only generates woff and woff2 fonts and matching css - that's enough for the modern web. 
+* The font contains only the given subset of icons - can save a lot of space.
+
+## Define, build, use
+
+### Generate a new, reduced mdi webfont
+1. Prerequisite: Have a working [Node.js](https://nodejs.org/) environment. 
+1. `cd` into this dir, run `npm install` or `yarn install` to load the dev dependencies.
+1. Add the names of the icons you want to the `./subset.json` file. Leave the top 3 there, they are required for the `./dist/index.html` sample page.
+1. `npm build` or `yarn build` creates the fonts, css, and sample page in the `./dist` folder. 
+1. Optional: `cd dist`, run `http-server` and check the sample page containing your icon selection at `http://localhost:8080`.
+
+### Use the font in an app
+1. From `./dist`, copy the `css` and `fonts` folder as peers into the app assets folder.
+1. Include the `css/materialdesignicons.css` into the app css structure.
+1. Accessing the icons in the reduced webfont is the same as with the full mdi font.
+
+## Notes
+* The `./bin/index.js` is an adapted version of the original `@mdi/font-build` package.
+* The package is basically a clone of the original `@mdi/font-build` package. 2 files have been slightly adapted:
+  * `./bin/index.js`
+  * `./src/scss/_path.scss`
+* In addition to `./dist`, the build also creates an `./icon` folder that contains the specified icon svg files. You can safely delete that folder after a successful build.
+
+## Credits
+Full credits go to the creator and contributors of the [@mdi/font-build](https://www.npmjs.com/package/@mdi/font-build) package. My additions make it worse.
---
+++ bin/index.js
@@ -0,0 +1,333 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs')
+const webfont = require('webfont').default
+const sass = require('node-sass')
+const fs = require('fs')
+const path = require('path')
+
+const argv = yargs
+  .option('dir', {
+    description: 'Path to ./meta.json and ./font-build.json and ./svg folder with SVG files',
+    default: './node_modules/@mdi/svg',
+    type: 'string'
+  })
+  .option('meta', {
+    description: 'Instead of --dir give path to ./meta.json',
+    default: './meta.json',
+    type: 'string'
+  })
+  .option('subset', {
+    description: 'Path to ./subset.json',
+    default: './subset.json',
+    type: 'string'
+  })
+  .option('font', {
+    description: 'Instead of --dir give path to ./font-build.json',
+    default: './font-build.json',
+    type: 'string'
+  })
+  .option('svg', {
+    description: 'Instead of --dir give path to ./svg folder',
+    default: './svg',
+    type: 'string'
+  })
+  .option('dist', {
+    description: 'Override ./dist folder for exported files',
+    default: './dist',
+    type: 'string'
+  })
+  .option('fontSvg', {
+    description: 'Include SVG font in output',
+    default: false,
+    type: 'boolean'
+  })
+  .help()
+  .alias('help', 'h')
+  .version()
+  .argv
+
+const currentPath = process.cwd()
+const distFolder = path.resolve(currentPath, argv.dist)
+let metaFile = path.resolve(currentPath, argv.dir, 'meta.json')
+let subsetFile = path.resolve(currentPath, 'subset.json')
+let fontBuildFile = path.resolve(currentPath, argv.dir, 'font-build.json')
+let svgFolder = path.resolve(currentPath, argv.dir, 'svg')
+if (argv.meta !== './meta.json') {
+  // Use --meta ./otherfile.json
+  metaFile = path.resolve(currentPath, argv.meta)
+}
+if (argv.subset !== './subset.json') {
+  // Use --subset ./otherfile.json
+  subsetFile = path.resolve(currentPath, argv.subset)
+}
+if (argv.font !== './font-build.json') {
+  // Use --font ./otherfile.json
+  fontBuildFile = path.resolve(currentPath, argv.font)
+}
+if (argv.svg !== './svg') {
+  // Use --svg ./other/directory/svg
+  svgFolder = path.resolve(currentPath, argv.svg)
+}
+
+if (!fs.existsSync(fontBuildFile)) {
+  console.error(`Unable to find "${fontBuildFile}". Use --help to learn more.`)
+  process.exit(1)
+}
+
+if (!fs.existsSync(metaFile)) {
+  console.error(`Unable to find "${metaFile}". Use --help to learn more.`)
+  process.exit(1)
+}
+
+if (!fs.existsSync(subsetFile)) {
+  console.error(`Unable to find "${subsetFile}". Use --help to learn more.`)
+  process.exit(1)
+}
+
+if (!fs.existsSync(svgFolder)) {
+  console.error(`Unable to find "${svgFolder}" folder. Use --help to learn more.`)
+  process.exit(1)
+}
+
+const fontBuildString = fs.readFileSync(fontBuildFile)
+const fontBuildJson = {
+  prefix: 'default',
+  name: 'default name',
+  icon: 'M0,0H8V3H18V0H26V8H23V18H26V26H18V23H8V21H18V18H21V8H18V5H8V8H5V18H8V26H0V18H3V8H0V0M2,2V6H6V2H2M2,20V24H6V20H2M20,2V6H24V2H20M20,20V24H24V20H20Z',
+  ...JSON.parse(fontBuildString)
+}
+// get our subset, a simple array of icon names
+const subsetString = fs.readFileSync(subsetFile)
+const subsetArray = JSON.parse(subsetString)
+// filter the full original meta.json by our selection
+const metaString = fs.readFileSync(metaFile)
+const metaJson = JSON.parse(metaString).filter(icon =>
+  subsetArray.includes(icon.name)
+)
+
+console.log(`Generating from ${metaJson.length} icons:`)
+
+let errors = []
+
+metaJson.forEach(icon => {
+  if (errors.length > 5) { return }
+  const newFile = path.join(svgFolder, `u${icon.codepoint}-${icon.name}.svg`)
+  if (fs.existsSync(newFile)) {
+    // Already Converted, so quick check
+    return
+  }
+  const oldFile = path.join(svgFolder, `${icon.name}.svg`)
+  if (fs.existsSync(oldFile)) {
+    fs.renameSync(oldFile, newFile)
+  } else {
+    errors.push(`Invalid icon at "${oldFile}"`)
+  }
+})
+
+if (errors.length) {
+  errors.forEach(error => {
+    console.error(error)
+  })
+  console.error('etc...')
+  process.exit(1)
+}
+
+function generateFolders () {
+  if (!fs.existsSync(distFolder)) {
+    fs.mkdirSync(distFolder, { recursive: true })
+  }
+  const folders = ['scss', 'css', 'fonts']
+  folders.forEach(folder => {
+    if (!fs.existsSync(path.join(distFolder, folder))) {
+      fs.mkdirSync(path.join(distFolder, folder))
+    }
+  })
+  console.log('- Folders created.')
+}
+
+function generateIndex () {
+  const {
+    prefix,
+    name,
+    icon,
+    fileName,
+    fontName,
+    fontFamily,
+    fontWeight,
+    version,
+    npmFont,
+    npmJS,
+    npmSVG,
+    website
+  } = fontBuildJson
+  const htmlSrc = path.resolve(__dirname, '..', 'src', 'index.html')
+  const htmlDist = path.resolve(distFolder, 'index.html')
+  const icons = []
+  metaJson.forEach(icon => {
+    const deprecated = icon.deprecated ? ',deprecated:true' : ''
+    icons.push(`{name:"${icon.name}",hex:"${icon.codepoint}",version:"${icon.version}"${deprecated}}`)
+  })
+  const htmlString = fs.readFileSync(htmlSrc, 'utf8')
+    .replace(/prefix/g, prefix)
+    .replace(/packageName/g, name)
+    .replace(/packageIcon/g, icon)
+    .replace(/fileName/g, fileName)
+    .replace(/fontName/g, fontName)
+    .replace(/fontFamily/g, fontFamily)
+    .replace(/fontWeight/g, fontWeight)
+    .replace(/-.-.-/g, `${version.major}.${version.minor}.${version.patch}`)
+    .replace(/npmFont/g, npmFont)
+    .replace(/npmJS/g, npmJS)
+    .replace(/npmSVG/g, npmSVG)
+    .replace(/domain\.com/g, website)
+    .replace(/icons = \[\]/, `icons = [${icons.join(',')}]`)
+  fs.writeFileSync(htmlDist, htmlString)
+  console.log('- Generated index.html')
+}
+
+function generateSCSS () {
+  const {
+    prefix,
+    fileName,
+    fontName,
+    fontFamily,
+    fontWeight,
+    version,
+    website
+  } = fontBuildJson
+  // {fileName}.scss
+  const main = path.resolve(__dirname, '..', 'src', 'scss', 'main.scss')
+  const mainDist = path.resolve(distFolder, 'scss', `${fileName}.scss`)
+  const mainString = fs.readFileSync(main, 'utf8')
+    .replace(/domain\.com/, website)
+  fs.writeFileSync(mainDist, mainString)
+  console.log(`- Generated ${fileName}.scss`)
+  // Others
+  const others = [
+    'animated',
+    'core',
+    'extras',
+    'functions',
+    'icons',
+    'path',
+    'variables'
+  ]
+  others.forEach(file => {
+    const other = path.resolve(__dirname, '..', 'src', 'scss', `_${file}.scss`)
+    const otherDist = path.resolve(distFolder, 'scss', `_${file}.scss`)
+    let otherString = fs.readFileSync(other, 'utf8')
+      .replace(/prefix/g, prefix)
+      .replace(/fileName/g, fileName)
+      .replace(/fontName/g, fontName)
+      .replace(/fontFamily/g, fontFamily)
+      .replace(/fontWeight/g, fontWeight)
+      .replace(/-.-.-/g, `${version.major}.${version.minor}.${version.patch}`)
+      .replace(new RegExp(`${prefix}-css-${prefix}`, 'g'), `${prefix}-css-prefix`)
+    if (file === 'variables') {
+      const icons = []
+      metaJson.forEach(icon => {
+        icons.push(`  "${icon.name}": ${icon.codepoint}`)
+      })
+      otherString = otherString.replace(/icons: \(\)/, `icons: (\n${icons.join(',\n')}\n)`)
+    }
+    fs.writeFileSync(otherDist, otherString)
+    console.log(`  - Generated _${file}.scss`)
+  })
+}
+
+function generateCSS () {
+  const { fileName } = fontBuildJson
+  sass.render({
+    file: path.resolve(distFolder, 'scss', `${fileName}.scss`),
+    outputStyle: 'expanded',
+    sourceMap: true,
+    outFile: `${fileName}.css`
+  }, function (err, result) {
+    if (err) {
+      console.error(err)
+    } else {
+      fs.writeFileSync(path.join(distFolder, 'css', `${fileName}.css`), result.css)
+      const mapData = Buffer.from(result.map.toString('utf8').replace(/dist\//g, '../'))
+      fs.writeFileSync(path.join(distFolder, 'css', `${fileName}.css.map`), mapData)
+    }
+  })
+  sass.render({
+    file: path.resolve(distFolder, 'scss', `${fileName}.scss`),
+    outputStyle: 'compressed',
+    sourceMap: true,
+    outFile: `${fileName}.css`
+  }, function (err, result) {
+    if (err) {
+      console.error(err)
+    } else {
+      fs.writeFileSync(path.join(distFolder, 'css', `${fileName}.min.css`), result.css)
+      const mapData = Buffer.from(result.map.toString('utf8').replace(/dist\//g, '../'))
+      fs.writeFileSync(path.join(distFolder, 'css', `${fileName}.min.css.map`), mapData)
+    }
+  })
+  console.log(`- Generated ${fileName}.css / *.min.css / *.map`)
+}
+
+// copy the chosen .svg files from the @mdi/svg package to the empty ./icon working folder here
+const iconFolder = path.resolve(currentPath, 'icon')
+if (fs.existsSync(iconFolder)) {
+  fs.rmdirSync(iconFolder, { recursive: true, force: true })
+}
+fs.mkdirSync(iconFolder)
+
+errors = []
+metaJson.forEach(icon => {
+  if (errors.length > 5) {
+    return
+  }
+  const srcFile = path.join(svgFolder, `u${icon.codepoint}-${icon.name}.svg`)
+  if (fs.existsSync(srcFile)) {
+    const destFile = path.join(
+      iconFolder,
+      `u${icon.codepoint}-${icon.name}.svg`
+    )
+    fs.copyFileSync(srcFile, destFile)
+  } else {
+    errors.push(`Invalid icon at "${srcFile}"`)
+  }
+})
+if (errors.length) {
+  errors.forEach(error => {
+    console.error(error)
+  })
+  console.error('etc...')
+  process.exit(1)
+}
+
+const formats = ['woff', 'woff2']
+if (argv.fontSvg) {
+  formats.push('svg')
+}
+
+webfont({
+  files: 'icon/*.svg',
+  fontName: fontBuildJson.fontName,
+  formats: formats,
+  fontHeight: 512,
+  descent: 64
+})
+  .then(result => {
+    const { fileName } = fontBuildJson
+    generateFolders()
+    // fs.writeFileSync(path.join(distFolder, 'fonts', `${fileName}-webfont.ttf`), result.ttf)
+    // fs.writeFileSync(path.join(distFolder, 'fonts', `${fileName}-webfont.eot`), result.eot)
+    fs.writeFileSync(path.join(distFolder, 'fonts', `${fileName}-webfont.woff`), result.woff)
+    fs.writeFileSync(path.join(distFolder, 'fonts', `${fileName}-webfont.woff2`), result.woff2)
+    if (argv.fontSvg) {
+      fs.writeFileSync(path.join(distFolder, 'fonts', `${fileName}-webfont.svg`), result.svg)
+    }
+    console.log(`- Generated woff, and woff2${argv.fontSvg && ' + svg'}`)
+    generateIndex()
+    generateSCSS()
+    generateCSS()
+    return result
+  })
+  .catch(error => {
+    throw error
+  })
